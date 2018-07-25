038fdcfef2f9063603167dd42fbacd37028b0259
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-07-25 09:46:20 +0900

    carrierwave no mae

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     users_search_url
   end
 
     private
-        def sign_in_required
-            redirect_to new_user_session_url unless user_signed_in?
-            flash[:notice]="You have to log in" unless user_signed_in?
-        end
+      def sign_in_required
+          redirect_to new_user_session_url unless user_signed_in?
+          flash[:notice]="You have to log in" unless user_signed_in?
+      end
+
+    protected
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      end
 end
