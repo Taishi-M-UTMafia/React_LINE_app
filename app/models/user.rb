cce82d417cbed6b7a30f69f06d52eaf7a38b7eab
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-07-25 17:21:06 +0900

    set validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
+  # Himotsuke
   has_many :friendships_of_from_user, :class_name => 'Friendship', :foreign_key => 'from_user_id', :dependent => :destroy
   has_many :friendships_of_to_user, :class_name => 'Friendship', :foreign_key => 'to_user_id', :dependent => :destroy
   has_many :friends_of_from_user, :through => :friendships_of_from_user, :source => 'to_user'
   has_many :friends_of_to_user, :through => :friendships_of_to_user, :source => 'from_user'
+
+  # validation
+  validates :email, {presence:true}
+  validates :password, {presence:true}
+  validates :name, {presence:true}
+  validates :image_name, {presence:true}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
