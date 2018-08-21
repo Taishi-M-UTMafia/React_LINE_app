288db05a00ef566a3bf54d1a61ac364461e0bcf0
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-08-21 10:50:57 +0900

    bichousei

--- app/assets/javascripts/src/components/users/searchForm.js
+++ app/assets/javascripts/src/components/users/searchForm.js
@@ -40,12 +40,11 @@ export default class SearchForm extends React.Component {
   }
 
   createFriend(toUserId) {
-    // location.href = 'http://localhost:3000'
+    location.href = 'http://localhost:3000'
     FriendshipAction.createFriend(toUserId)
   }
   render() {
-    // 治してね
-    const userLists = this.state.users.map((user) => {
+    const userLists = this.state.users.slice(0,5).map((user) => {
       return (
         <div
           className='search_user_list_result'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :sign_in_required
   def search
   end
 end
